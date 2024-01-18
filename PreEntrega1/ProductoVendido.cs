3dc7a4281ca06ef6fc938c502bd6fc89d88f319d
José Rivera <jmrivera82@gmail.com>
2024-01-17 21:40:53 -0300

    Clases preentrega1

---
+++ PreEntrega1/ProductoVendido.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreEntrega1
+{
+    public class ProductoVendido
+    {
+        public int Id { get; set; } 
+
+        public int IdProducto { get; set; }
+
+        public double Stock { get; set;}
+
+        public int IdVenta { get; set; }    
+    }
+}
---
+++ PreEntrega1/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreEntrega1
+{
+    public class Usuario
+    {
+        public int Id { get; set; } 
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set;}
+
+        public string NombreUsuario { get; set;}    
+        public string Contraseña { get; set;}
+
+        public string Mail { get; set;}
+
+    }
+}
