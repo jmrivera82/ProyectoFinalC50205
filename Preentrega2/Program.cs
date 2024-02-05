4ff3229772e2a7012da5547b7a8faf39fd0eaf2f
José Rivera <jmrivera82@gmail.com>
2024-02-05 12:34:51 -0300

    desafio 2

---
+++ Preentrega2/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+internal class Program
+{
+
+    public static void Main(string[] args)
+
+
+    {
+
+        int opcion;
+
+        Console.WriteLine("Menu Programa");
+        Console.WriteLine("1.- Gestión 1");
+        Console.WriteLine("2.- Gestión 2 ");
+        Console.WriteLine("3.- Gestión 3 ");
+        Console.WriteLine("4.- Gestión 4 ");
+
+
+        Console.WriteLine("Por favor ingresar opcion: ");
+        opcion = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Se ingresó la opción: "+opcion);
+
+        switch (opcion)
+        {
+            case 1:
+                Console.WriteLine("opcion 1");
+                break;
+
+            case 2:
+                Console.WriteLine("opcion 2");
+                break;
+
+            case 3:
+                Console.WriteLine("opcion 3");
+                break;
+
+            case 4:
+                Console.WriteLine("opcion 4");
+                break;
+
+
+
+
+        }
+
+
+
+    }
+
+}
+
+
+
+
+
+
+
---
+++ Preentrega2/database/GestorDeBaseDeDatos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Preentrega2.database
+{
+    public class GestorDeBaseDeDatos
+    {
+        private string stringConnection;
+
+        public GestorDeBaseDeDatos()
+        {
+            this.stringConnection = "Server=localhost\\SQLEXPRESS;Database=proyecto;Trusted_Connection=True;";
+        }
+    }
+}
